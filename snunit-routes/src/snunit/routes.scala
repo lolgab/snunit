7a3b93192d011d10f7cb5a81b600d95e48be2bde
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-09-29 17:49:17 +0200

    Use parseArgs instead of unapply (#18)

--- snunit-routes/src/snunit/routes.scala
+++ snunit-routes/src/snunit/routes.scala
@@ -7,7 +7,7 @@ object routes {
   implicit class ServerBuilderOps[T <: ServerBuilder](private val builder: T) extends AnyVal {
     def withRoute[Args](route: Route[Args])(handler: (Request, Args) => Unit): T = {
       builder.withRequestHandler(req =>
-        route.unapply(req.path) match {
+        route.parseArgs(req.path) match {
           case Some(args) => handler(req, args)
           case None       => req.next()
         }
