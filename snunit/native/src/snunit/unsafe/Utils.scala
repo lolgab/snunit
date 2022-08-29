5360a6cf9eed77fd80b1b13977f914bd1362c386
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-08-29 18:35:26 +0200

    Use memcpy in fromCStringAndSize (#88)
    * Use memcpy in fromCStringAndSize
    
    * Return empty string when size is 0

--- snunit/native/src/snunit/unsafe/Utils.scala
+++ snunit/native/src/snunit/unsafe/Utils.scala
@@ -1,20 +1,21 @@
 package snunit.unsafe
 
 import java.nio.charset.Charset
+import scala.scalanative.libc.string.memcpy
+import scala.scalanative.runtime.ByteArray
 import scala.scalanative.unsafe._
+import scala.scalanative.unsigned._
 
 private[snunit] object Utils {
   private val charset = Charset.defaultCharset()
 
   def fromCStringAndSize(cstr: CString, size: Int): String = {
-    val bytes = new Array[Byte](size)
+    if (size > 0) {
+      val bytes = new Array[Byte](size)
 
-    var c = 0
-    while (c < size) {
-      bytes(c) = !(cstr + c)
-      c += 1
-    }
+      memcpy(bytes.asInstanceOf[ByteArray].at(0), cstr, size.toULong)
 
-    new String(bytes, charset)
+      new String(bytes, charset)
+    } else ""
   }
 }
