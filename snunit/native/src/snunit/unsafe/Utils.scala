6d63d0696578e265a795e8abc8c48e284bd12871
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-09-22 15:04:53 +0200

    Avoid CSize in memcpy by defining extern object

--- snunit/native/src/snunit/unsafe/Utils.scala
+++ snunit/native/src/snunit/unsafe/Utils.scala
@@ -1,19 +1,22 @@
 package snunit.unsafe
 
 import java.nio.charset.Charset
-import scala.scalanative.libc.string.memcpy
 import scala.scalanative.runtime.ByteArray
 import scala.scalanative.unsafe._
-import scala.scalanative.unsigned._
 
 private[snunit] object Utils {
+  @extern
+  private object libc {
+    // to avoid unsigned number in Scala Native's libc
+    def memcpy(dest: Ptr[Byte], src: Ptr[Byte], size: Long): Ptr[Byte] = extern
+  }
   private val charset = Charset.defaultCharset()
 
   def fromCStringAndSize(cstr: CString, size: Int): String = {
     if (size > 0) {
       val bytes = new Array[Byte](size)
 
-      memcpy(bytes.asInstanceOf[ByteArray].at(0), cstr, size.toULong)
+      libc.memcpy(bytes.asInstanceOf[ByteArray].at(0), cstr, size)
 
       new String(bytes, charset)
     } else ""
