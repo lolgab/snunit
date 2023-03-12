f3a4c836c5e0b628c611343211efc7bee4e18962
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-03-12 16:59:52 +0100

    Update plugins tests to new API

--- snunit-mill-plugin-itest/src/simple/module/src/Main.scala
+++ snunit-mill-plugin-itest/src/simple/module/src/Main.scala
@@ -3,7 +3,8 @@ import snunit._
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     SyncServerBuilder
-      .build(_.send(StatusCode.OK, "Hello world", Seq()))
+      .setRequestHandler(_.send(StatusCode.OK, "Hello world", Seq()))
+      .build()
       .listen()
   }
 }
