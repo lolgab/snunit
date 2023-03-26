80ad4f8474c9da3145ced401ccb2fbe8741cb132
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-03-26 01:29:36 +0100

    Update plugin tests

--- sbt-plugin/src/sbt-test/snunit-plugin/simple/src/main/scala/Main.scala
+++ sbt-plugin/src/sbt-test/snunit-plugin/simple/src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import snunit._
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     SyncServerBuilder
-      .setRequestHandler(_.send(StatusCode.OK, "Hello world", Seq()))
+      .setRequestHandler(_.send(StatusCode.OK, "Hello world", Seq.empty[String, String]))
       .build()
       .listen()
   }
