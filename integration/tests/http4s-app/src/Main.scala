2245446877b2fed2a771f3a73e1040bf9e62e100
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-06-04 21:09:54 +0200

    Try to reproduce hanging app

--- integration/tests/http4s-app/src/Main.scala
+++ integration/tests/http4s-app/src/Main.scala
@@ -3,13 +3,15 @@ package snunit.tests
 import cats.effect._
 import org.http4s._
 import org.http4s.dsl.io._
+import cats.syntax.all.{*, given}
 
-object Main extends snunit.Http4sApp {
-  def routes = Resource.pure(
-    HttpRoutes
-      .of[IO] { case GET -> Root =>
-        Ok("Hello Http4s App!")
-      }
-      .orNotFound
+object Main extends snunit.Http4sApp:
+  final def routes = Resource.eval(IO(println("starting server"))) *> Resource.eval(
+    IO(
+      HttpRoutes
+        .of[IO] { case req =>
+          IO(println((s"Responding to $req"))) *> Ok("Hello Http4s App!")
+        }
+        .orNotFound
+    )
   )
-}
