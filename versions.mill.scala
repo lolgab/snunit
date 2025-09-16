4585f379fc7ad399fb3ad1d8a5c6c5ad70ce6e83
Anton Sviridov <keynmol@gmail.com>
2025-09-16 09:15:11 +0100

    Experiment with SN 0.5 in http4s

--- versions.mill.scala
+++ versions.mill.scala
@@ -3,15 +3,15 @@ package build
 // This file is system-linked in sbt-plugin/project/Versions.scala
 // It needs to be a valid Scala file so we can't use top level `val`s
 object Versions {
-  val scalaNative = "0.5.7"
+  val scalaNative = "0.5.8"
   val upickle = "4.0.2"
   val undertow = "2.3.18.Final"
   val scala212 = "2.12.19"
-  val scala3 = "3.3.4"
-  val tapir = "1.11.1"
+  val scala3 = "3.3.6"
+  val tapir = "1.11.44"
   val cask = "0.10.2"
-  val catsEffect = "3.6-623178c"
-  val http4s023 = "0.23.26"
+  val catsEffect = "3.7.0-RC1"
+  val http4s023 = "0.23.30-161-f5b9629-SNAPSHOT"
   val http4s1 = "1.0.0-M41"
   val mill011 = "0.11.13"
   val utest = "0.8.4"
